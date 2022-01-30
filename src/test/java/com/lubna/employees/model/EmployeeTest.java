e1ff4c6d451f489d3642dc36978d009959066053
Lubna Helaly <85235991+LubnaHelalyD@users.noreply.github.com>
2022-01-30 20:07:19 +0200

    unit test

--- src/test/java/com/lubna/employees/model/EmployeeTest.java
+++ src/test/java/com/lubna/employees/model/EmployeeTest.java
@@ -1,2 +1,22 @@
-package com.lubna.employees.model;public class EmployeeTest {
+package com.lubna.employees.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmployeeTest {
+
+    private Employee e;
+
+    @Before
+    public void init(){
+        e = new Employee("567","77","2011-04-22","2022-06-29");
+    }
+
+    @Test
+    public void SetterAndGetters_Success() {
+        Assert.assertNotNull(e);
+        Assert.assertEquals(e.getEmployeeID(),"567");
+        Assert.assertEquals(e.getProjectID(),"77");
+    }
 }
--- src/test/java/com/lubna/employees/model/EmployeeWorkTogetherTest.java
+++ src/test/java/com/lubna/employees/model/EmployeeWorkTogetherTest.java
@@ -1,2 +1,22 @@
-package com.lubna.employees.model;public class EmployeeWorkTogetherTest {
+package com.lubna.employees.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmployeeWorkTogetherTest {
+
+    private EmployeeWorkTogether employeeWorkTogether;
+
+    @Before
+    public void init(){
+        employeeWorkTogether = new EmployeeWorkTogether("55","66","1", 9);
+    }
+
+    @Test
+    public void SetterAndGetters_Success() {
+        Assert.assertNotNull(employeeWorkTogether);
+        Assert.assertEquals(employeeWorkTogether.getDaysWorked(), 9);
+        Assert.assertEquals(employeeWorkTogether.getProjectId(), "1");
+    }
 }
